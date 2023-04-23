67bbdeca4f8910d249984cb911a7af340ef81f98
mona <monasief833@gmail.com>
2023-04-23 16:34:03 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
-int main(void){
+/**
+ * main -Entry point 
+ * 
+ * Description: A C program that print with puts function
+ *
+ * Return always: 0 (success)
+*/ 
+
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
 }
