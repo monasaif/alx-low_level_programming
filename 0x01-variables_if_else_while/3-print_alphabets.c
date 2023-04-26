a3aae9fc7f2c065eb66fb14063719b69b2a7e4b9
mona <monasief833@gmail.com>
2023-04-26 16:16:27 -0700

    ALPH UP AND LOE

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print alphabet lowercase then in uppercase using putchar
+ *
+ * Return: always 0 (success)
+*/
+
+int main(void)
+{
+	char ch = 'a';
+	char CH = 'A';
+
+	/*prints a - z*/
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+
+	/*print A - Z*/
+	while (CH <= 'Z')
+	{
+		putchar(CH);
+		CH++
+	}
+	putchar('\n');
+
+	return (0);
+}
