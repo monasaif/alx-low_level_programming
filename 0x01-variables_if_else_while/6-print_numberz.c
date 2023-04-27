986a45bb4f7beae8cff409990d90a9254883f701
mona <monasief833@gmail.com>
2023-04-26 17:29:10 -0700

    print nim

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print numbers using putchar
+ *
+ * Return: always 0 (success)
+*/
+
+int main(void)
+{
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+		putchar(digit + '0');
+		digit++;
+	}
+	putchar('\n');
+
+	return (0);
+}
