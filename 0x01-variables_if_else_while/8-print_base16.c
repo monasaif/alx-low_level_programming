2f6c3d9e280fdae0ca21b7a122633d61e2528fef
mona <monasief833@gmail.com>
2023-04-26 18:18:30 -0700

    base 16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,21 +10,15 @@
 
 int main(void)
 {
-	int digit = 0;
+	int digit = 48;
 
-	while (digit <= 9)
+	while (digit <= 102)
 	{
 		putchar(digit);
+
+		if (digit == 57)
+			digit += 39;
 		digit++;
-	}
-	putchar('\n');
-
-	char ch = 'a';
-
-	while (ch <= 'f')
-	{
-		putchar(ch);
-		ch++;
 	}
 	putchar('\n');
 
