a58c722cf8ff69699bf64971804b51f10b79831e
mona <monasief833@gmail.com>
2023-05-01 16:06:54 -0700

    print alph

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * print_alphabet - use _putchar function to print a -z
+ * print_alphabet - use _putchar function to print a - z
+ *
 */
 
 void print_alphabet(void)
@@ -9,6 +10,6 @@ void print_alphabet(void)
 	int ch;
 
 	for (ch = 'a'; ch <= 'z'; ch++)
-		-putchar(ch);
+		_putchar(ch);
 	_putchar('\n');
 }
