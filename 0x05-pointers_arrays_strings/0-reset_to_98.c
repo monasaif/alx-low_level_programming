a453e7a31e5ba2f280c05af36b2e3266a45e4b4e
mona <monasief833@gmail.com>
2023-05-02 17:03:45 -0700

    rest to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes apointer to an int parameter and update value to 98
+ *
+ * @n: int parameter
+ *
+ * return: nothing
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
