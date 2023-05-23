691a816c913beec487813ad5580a09021bb43e16
mona <monasief833@gmail.com>
2023-05-23 16:41:15 -0700

    dog itialize

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * init_dog -  initialize a variable of type
+ * @d: pointer
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ * Return: void
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
