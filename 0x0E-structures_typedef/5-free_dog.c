3952fa02c21e0d5f8573aa1098b767fd6a128201
mona <monasief833@gmail.com>
2023-05-23 18:22:58 -0700

    free dogs

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * free_dog - free dog
+ * @d: pointer
+ *
+ * Return: void
+*/
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		if (d->name)
+			free(d->name);
+		if (d->owner)
+			free(d->owner);
+		free(d);
+	}
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -22,5 +22,6 @@ typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 int _strlen(const char *str);
 char *_strcopy(char *dest, char *src);
+void free_dog(dog_t *d);
 
 #endif
