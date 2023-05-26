fdd599aaf9de9989048de8011ccbf037c78e99d4
mona <monasief833@gmail.com>
2023-05-25 19:15:44 -0700

    operators

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,5 +1,6 @@
-#include "3-cal.h"
+#include "3-calc.h"
 #include<stdio.h>
+#include <stdlib.h>
 
 /**
  * main - check the code.
@@ -13,14 +14,15 @@ int main(int argc, char **argv)
 	int (*op_func)(int, int), a, b;
 
 	if (argc != 4)
-		prinf("Error\n"), exit(98);
+		printf("Error\n"), exit(98);
 
 	a = atoi(argv[1]);
-	b = atio(argv[3]);
+	b = atoi(argv[3]);
 
-	op_func = get_on_func(argv[2]);
-	if (!op_finc)
+	op_func = get_op_func(argv[2]);
+	if (!op_func)
 		printf("Error\n"), exit(99);
+
 	if (!b && (argv[2][0] == '/' || argv[2][0] == '%'))
 		printf("Error\n"), exit(100);
 
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,5 +1,5 @@
 #include "3-calc.h"
-#include <stdio>
+#include <stdio.h>
 
 /**
  * op_add - add two integer
