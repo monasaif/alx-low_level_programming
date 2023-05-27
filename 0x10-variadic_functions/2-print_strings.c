e6dec4c530efd2ebef0ee5c3af83d3fd3eb2ef30
mona <monasief833@gmail.com>
2023-05-26 19:37:50 -0700

    prints

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -20,7 +20,6 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		printf("\n");
 		return;
 	}
-
 	va_start(ap, n);
 	while (i--)
 		printf("%s%s", (str = va_arg(ap, char *)) ? str : "(nil",
